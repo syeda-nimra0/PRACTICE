153191ce92acb371e71503930c97728884509578
syeda-nimra0 <nimrasyeda37@gmail.com>
2025-08-01 11:41:32 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+// //  if statement
+// let sunny = true;
+// let weekend = false;
+// if (sunny && weekend){
+//     alert("Its a picnic time");
+// }else{
+//     alert("Its not");
+// }
+// nested if statement
+// let loggedIn = true;
+// let isAdmin = false;
+// if(loggedIn){
+//     if(isAdmin){
+//         alert("welcome admin");
+//     }else{
+//         alert("welcome user");
+//     }
+// }else{
+//     alert("please create an account");
+// }
+// let age = +prompt("Enter your age");
+// let gender = prompt("mention your gender").toLocaleLowerCase();
+// if(age > 18 && gender === "Women"){
+//     alert("You can join the women's club");
+// }else{
+//     alert("Sorry you can not");
+// }
+// function
+
+// function greetUser(name){
+//     alert("HELLO " + name + "!");
+// }
+// greetUser("nimra");
+// greetUser("Ali");
+// greetUser("Ahsan");
+// function add(a, b){
+//     return a + b;
+// }
+// let sum = add(5, 3);
+// alert(sum);
+// let globalVar = "i am global";
+// function test(){
+//     let localVar = "i am local";
+//     alert(globalVar);
+//     alert(localVar);
+// }
+// test();
+// // alert(globalVar);
+// // alert(localVar);
+// function calculateBill(){
+//     let price = +prompt("enter your price");
+//     let quantity = +prompt("How many/ How much?");
+//     return price * quantity; 
+// }
+// let totalBill = calculateBill();
+// let Gst = totalBill * 0.10;
+// let finalBill = totalBill + Gst;
+// alert("Your final bill is " + finalBill);
+// function discountPrice(){
+//     let price = +prompt("enter price");
+//     let perc = +prompt("enter percentage of price");
+//     let finalPrice = price - (price * perc / 100);
+//     return finalPrice;
+// }
+// let out = discountPrice();
+// alert("your price " + out);
+
+ 
